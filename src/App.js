a83bfe3f94328cfb6b3585bd6e743b3e93632852
VietIS Tan <tan.hoangngoc@vietis.com.vn>
2021-02-26 17:40:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TD A</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
